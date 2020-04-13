3fc30b0582fdead6806248257319975c0e325e20
Florent Vicard <vicos51@gmail.com>
2020-04-13 15:15:53 +0200

    Refactor into ES private fields syntax

--- src/play.ts
+++ src/play.ts
@@ -1,46 +1,46 @@
 class Play {
-  readonly id: number;
-  readonly date: Date;
-  readonly quantity?: number;
-  readonly length?: number;
-  readonly incomplete: boolean;
-  readonly nowinstats: boolean;
-  readonly location?: string;
-  readonly gameName: string;
-  readonly gameId: number;
-  readonly players: Player[];
+  #id: number;
+  #date: Date;
+  #quantity?: number;
+  #length?: number;
+  #incomplete: boolean;
+  #nowinstats: boolean;
+  #location?: string;
+  #gameName: string;
+  #gameId: number;
+  #players: Player[];
 
   #thumbnail?: string;
   #image?: string;
 
   constructor(elt: GoogleAppsScript.XML_Service.Element) {
-    this.id = Number.parseInt(elt.getAttribute("id").getValue(), 10);
-    this.date = new Date(elt.getAttribute("date").getValue());
+    this.#id = Number.parseInt(elt.getAttribute("id").getValue(), 10);
+    this.#date = new Date(elt.getAttribute("date").getValue());
     if (elt.getAttribute("quantity").getValue() !== "0") {
-      this.quantity = Number.parseInt(
+      this.#quantity = Number.parseInt(
         elt.getAttribute("quantity").getValue(),
         10
       );
     }
     if (elt.getAttribute("length").getValue() !== "0") {
-      this.length = Number.parseInt(elt.getAttribute("length").getValue(), 10);
+      this.#length = Number.parseInt(elt.getAttribute("length").getValue(), 10);
     }
-    this.incomplete = elt.getAttribute("incomplete").getValue() === "1";
-    this.nowinstats = elt.getAttribute("nowinstats").getValue() === "1";
+    this.#incomplete = elt.getAttribute("incomplete").getValue() === "1";
+    this.#nowinstats = elt.getAttribute("nowinstats").getValue() === "1";
     if (elt.getAttribute("location").getValue() !== "") {
-      this.location = elt.getAttribute("location").getValue();
+      this.#location = elt.getAttribute("location").getValue();
     }
     const itemElt = elt.getChild("item");
-    this.gameName = itemElt.getAttribute("name").getValue();
-    this.gameId = Number.parseInt(
+    this.#gameName = itemElt.getAttribute("name").getValue();
+    this.#gameId = Number.parseInt(
       itemElt.getAttribute("objectid").getValue(),
       10
     );
 
-    this.players = [];
+    this.#players = [];
     const playersElt = elt.getChild("players");
     for (const playerElt of playersElt.getChildren("player")) {
-      this.players.push(new Player(playerElt));
+      this.#players.push(new Player(playerElt));
     }
   }
 
@@ -55,7 +55,7 @@ class Play {
 
   get sortedPlayers() {
     // TODO: guess the sort way for this game (i.e if higher score is better)
-    return this.players.sort((a, b) => {
+    return this.#players.sort((a, b) => {
       if (a.isWinner !== b.isWinner) {
         return a.isWinner ? -1 : 1;
       } else {
@@ -80,7 +80,38 @@ class Play {
   }
 
   get url() {
-    return `https://www.boardgamegeek.com/play/details/${this.id}`
+    return `https://www.boardgamegeek.com/play/details/${this.#id}`;
+  }
+
+  get id() {
+    return this.#id;
+  }
+  get date() {
+    return this.#date;
+  }
+  get quantity() {
+    return this.#quantity;
+  }
+  get length() {
+    return this.#length;
+  }
+  get incomplete() {
+    return this.#incomplete;
+  }
+  get nowinstats() {
+    return this.#nowinstats;
+  }
+  get location() {
+    return this.#location;
+  }
+  get gameName() {
+    return this.#gameName;
+  }
+  get gameId() {
+    return this.#gameId;
+  }
+  get players() {
+    return this.#players;
   }
   get thumbnail() {
     return this.#thumbnail;
